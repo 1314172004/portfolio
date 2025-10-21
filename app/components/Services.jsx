9400062dbe7bbcccd09fcf3640da29786ff2cb43
aswini <aswinichinraju@gmail.com>
2025-10-21 21:41:15 +0530

    Services1

---
+++ app/components/Services.jsx
@@ -0,0 +1,12 @@
+
+const Services = () => {
+  return (
+      <div className = "w-full px-[12%] py-10 scroll-mt-20" >
+        <h4 className="text-center mb-2 text-lg font-Ovo">What I Offer</h4>
+        <h2 className="text-center text-5xl font-Ovo">My Services</h2>
+      
+    </div>
+  )
+}
+
+export default Services
